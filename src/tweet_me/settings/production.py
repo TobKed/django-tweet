2af8dc8024016d390a913c78c5a74925c13f43c2
TobKed <tobiaszkedzierski@gmail.com>
2018-11-20 20:43:10 +0100

    make multiple setting files

--- src/tweet_me/settings/production.py
+++ src/tweet_me/settings/production.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd6aar+8-h-mqz+dsr31&9_u(8fvea$#!gt#q791x2qs$&7-z4@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
