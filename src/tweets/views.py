c6715900f72959e5abc102ff9ecd1d892e123cfd
TobKed <tobiaszkedzierski@gmail.com>
2018-11-21 19:55:48 +0100

    add tweet app deteail and list views

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import Tweet
 
 
-def tweet_detail_view(request, id=1):
-    return render(request, "tweets/detail_view.html")
+def tweet_detail_view(request, pk):
+    obj = Tweet.objects.get(id=pk)
+    context = {
+        "object": obj
+    }
+    return render(request, "tweets/detail_view.html", context)
 
 
 def tweet_list_view(request):
-    return render(request, "tweets/list_view.html")
+    queryset = Tweet.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "tweets/list_view.html", context)
