25c70b9be29aa950847fd9589607a73389007b01
TobKed <tobiaszkedzierski@gmail.com>
2018-11-25 18:30:22 +0100

    add jQuery ajax calls on tweet list template

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,10 +1,18 @@
 from rest_framework import generics
 from .serializers import TweetModelSerializer
 from tweets.models import Tweet
+from django.db.models import Q
 
 
 class TweetListAAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
 
-    def get_queryset(self):
-        return Tweet.objects.all()
+    def get_queryset(self, *args, **kwargs):
+        queryset = Tweet.objects.all()
+        query = self.request.GET.get("q")
+        if query is not None:
+            queryset = queryset.filter(
+                Q(content__icontains=query) |
+                Q(user__username__icontains=query)
+            )
+        return queryset
