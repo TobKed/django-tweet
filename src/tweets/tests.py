4162abf4cdcd3dbfbd0fa63477bf18f21091f55f
TobKed <tobiaszkedzierski@gmail.com>
2018-11-25 07:52:23 +0100

    add tweet test

--- src/tweets/tests.py
+++ src/tweets/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Tweet
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+class TweetModelTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.user = User.objects.create(username="Jack Nicolson")
+        cls.tweet = Tweet.objects.create(
+            user=User.objects.first(),
+            content="test content 123"
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.tweet.delete()
+        cls.user.delete()
+
+    def test_tweet_item(self):
+        self.assertEqual(self.tweet.content, "test content 123")
+        self.assertTrue(self.tweet.id == 1)
+        absolute_url = reverse("tweet:tweet-detail-view", kwargs={"pk": self.tweet.pk})
+        self.assertEqual(self.tweet.get_absolute_url(), absolute_url)
