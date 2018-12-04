ee947b2042acf90c2079770275d233dc4fcfedf5
TobKed <tobiaszkedzierski@gmail.com>
2018-12-04 06:27:45 +0100

    add UserProfile model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user        = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile', on_delete=models.CASCADE)
+    following   = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followed_by', blank=True)
+    # user.profile.following -- users I follow
+    # user.followed_by -- user that follow me -- reverse relationship
+
+    def __str__(self):
+        return str(self.following.all().count())
--- src/tweet_me/urls.py
+++ src/tweet_me/urls.py
@@ -24,8 +24,9 @@ from tweets.views import TweetListView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TweetListView.as_view(), name='home-view'),
+
     path('tweet/', include(('tweets.urls', 'tweets'), namespace='tweet')),
-    path('profiles/', include(('accounts.urls', 'profiles'), namespace='profile')),
+    path('', include(('accounts.urls', 'profiles'), namespace='profile')),
     path('api/tweet/', include(('tweets.api.urls', 'tweets'), namespace='tweet-api')),
 ]
 
