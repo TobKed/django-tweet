ae4d5eccceb947c8e970db75717f600d2bc211a5
TobKed <tobiaszkedzierski@gmail.com>
2018-12-05 06:21:58 +0100

    add follow button

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserDetailView
+from .views import UserDetailView, UserFollowView
 
 
 urlpatterns = [
     path('<str:username>/', UserDetailView.as_view(), name='user-detail-view'),
+    path('<str:username>/follow/', UserFollowView.as_view(), name='user-follow-view'),
 ]
