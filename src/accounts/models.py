526e62311a74ec818d74ac71cb6c8a3b6d3de2a2
TobKed <tobiaszkedzierski@gmail.com>
2018-12-05 06:41:35 +0100

    upgrade follow button

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+from django.urls import reverse_lazy
 
 class UserProfileManager(models.Manager):
     use_for_related_fields = True
@@ -13,6 +13,24 @@ class UserProfileManager(models.Manager):
         except:
             pass
         return qs
+
+    def toggle_follow(self, user, to_toggle_user):
+        user_profile, created = UserProfile.objects.get_or_create(user=user)  # (user_obj, true)
+        if to_toggle_user in user_profile.following.all():
+            user_profile.following.remove(to_toggle_user)
+            added = False
+        else:
+            user_profile.following.add(to_toggle_user)
+            added = True
+        return added
+
+    def is_following(self, user, followed_by_user):
+        user_profile, created = UserProfile.objects.get_or_create(user=user)  # (user_obj, true)
+        if created:
+            return False
+        if followed_by_user in user_profile.following.all():
+            return True
+        return False
 
 
 class UserProfile(models.Model):
@@ -29,3 +47,9 @@ class UserProfile(models.Model):
     def get_following(self):
         users = self.following.all()
         return users.exclude(username=self.user.username)
+
+    def get_follow_url(self):
+        return reverse_lazy("profile:user-follow-view", kwargs={"username": self.user.username})
+
+    def get_absolute_url(self):
+        return reverse_lazy("profile:user-detail-view", kwargs={"username": self.user.username})
